f75b2c881debff04260bb9c1ae96b7603e83e787
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-25 14:06:14 +0530

    erase fontawsome add

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -6,7 +6,7 @@ export const TodoItem = ({ todo, onDelete }) => {
         <div>
             <h4>{todo.title}</h4>
             <p>{todo.desc}</p>
-            <button className="btn btn-sm btn-danger" onClick ={()=>{onDelete(todo)}}>Delete</button>
+            <button className="btn btn-sm btn-danger" onClick ={()=>{onDelete(todo)}}><i className="fas fa-eraser"></i>Delete</button>
         </div>
             <hr />
             </>
